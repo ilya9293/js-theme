763279a453a08fb344687a7656e38f8ab2f8307a
ilya9293 <ilya9293@ukr.net>
2022-01-29 15:32:05 +0200

    добавил разметку

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './sass/main.scss';
+
+const bodyRef = document.querySelector("body");
+const toolBar = document.querySelector(".theme-switch__control");
+
+toolBar.addEventListener("change", onChangeTheme);
+
+function onChangeTheme () {
+bodyRef.classList.toggle("dark-theme");
+}
